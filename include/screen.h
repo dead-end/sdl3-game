936a1edf40056d618cdae045b92ae4c287a39e18
Dead End <dead.end@inbox.ru>
2026-09-20 11:25:48 +0200

    comment

---
+++ include/screen.h
@@ -0,0 +1,27 @@
+#ifndef SCREEN_H
+#define SCREEN_H
+
+#include <SDL3/SDL.h>
+
+typedef struct Screen
+{
+
+    bool (*init)(void *state, SDL_Renderer *renderer);
+
+    SDL_AppResult (*event)(void *state, SDL_Event *event);
+
+    SDL_AppResult (*update)(void *state, double delta_time);
+
+    SDL_AppResult (*render)(void *state, SDL_Renderer *renderer);
+
+    void (*cleanup)(void *state);
+
+    //
+    // Screen specific data
+    //
+    void *state;
+} Screen;
+
+Screen StartScreen_Create(void);
+
+#endif
