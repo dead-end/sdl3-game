e2527857a70509c9a09c0801162a296626b5ece5
Dead End <dead.end@inbox.ru>
2026-09-21 23:45:17 +0200

    update

--- src/main.c
+++ src/main.c
@@ -1,159 +1,215 @@
-//
-// Activates SDL3 callback system
-//
-#define SDL_MAIN_USE_CALLBACKS 1
-
 #include <SDL3/SDL.h>
 #include <SDL3/SDL_main.h>
-
 #include <stdio.h>
+#include <stdbool.h>
+#include <emscripten.h>
+
 #include "screen_manager.h"
+#include "log.h"
 
 typedef struct
 {
     SDL_Window *window;
     SDL_Renderer *renderer;
-    ScreenManager screen_manager;
     uint64_t last_time;
 } AppContext;
 
 /**
- * By default emsdl uses stderr to write logs. This is mapped to
- * console.error(). Using printf writes to console.log().
+ * The function initializes the application.
  */
-void LogOutputFunction(void *userdata __attribute__((unused)), int category __attribute__((unused)), SDL_LogPriority priority __attribute__((unused)),
-                       const char *message)
+static SDL_AppResult _app_init(AppContext **ctx)
 {
-    printf("%s\n", message);
-}
 
-/**
- * SDL callback function to initialize the app.
- */
-SDL_AppResult SDL_AppInit(void **appstate,
-                          int argc __attribute__((unused)), char *argv[] __attribute__((unused)))
-{
-    //
-    // Register our LogOutputFunction to log to console.log()
-    //
-    SDL_SetLogOutputFunction(LogOutputFunction, NULL);
+    log_init();
 
-    //
-    // Allocate AppState
-    //
-    AppContext *ctx = SDL_calloc(1, sizeof(AppContext));
-    if (!ctx)
+    if (ctx == NULL)
     {
-        SDL_Log("Unable to allocate memory for AppState: %s", SDL_GetError());
+        SDL_Log("Invalid AppContext pointer.");
         return SDL_APP_FAILURE;
     }
-    *appstate = ctx;
 
-    //
-    // Initialize SDL video
-    //
+    *ctx = (AppContext *)SDL_calloc(1, sizeof(AppContext));
+    if (*ctx == NULL)
+    {
+        SDL_Log("Unable to allocate AppContext");
+        return SDL_APP_FAILURE;
+    }
+
     if (!SDL_Init(SDL_INIT_VIDEO))
     {
-        SDL_Log("Unable to initialize SDL: %s", SDL_GetError());
+        SDL_Log("Unable to initialize video: %s", SDL_GetError());
         return SDL_APP_FAILURE;
     }
 
-    //
-    // Create window and renderer
-    //
-    if (!SDL_CreateWindowAndRenderer("SDL3 WASM Game", 800, 600, 0, &ctx->window, &ctx->renderer))
+    if (!SDL_CreateWindowAndRenderer("SDL3 Game", 800, 600, 0, &(*ctx)->window, &(*ctx)->renderer))
     {
-        SDL_Log("Unable to create window and renderer: %s", SDL_GetError());
+        SDL_Log("Unable to initialize window / renderer: %s", SDL_GetError());
         return SDL_APP_FAILURE;
     }
 
-    ctx->last_time = SDL_GetTicks();
+    (*ctx)->last_time = SDL_GetTicks();
 
-    sm_init(&ctx->screen_manager, ctx->renderer);
+    sm_init((*ctx)->renderer);
 
     return SDL_APP_CONTINUE;
 }
 
 /**
- * SDL callback function to process events
+ * Cleanup function.
+ *
+ * Do not call SDL_Log after SDL_Quit
  */
-SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event *event)
+static void _app_cleanup(AppContext **ctx)
 {
-    if (event->type == SDL_EVENT_QUIT)
+    if (ctx && *ctx)
     {
-        return SDL_APP_SUCCESS;
+
+        sm_cleanup();
+
+        if ((*ctx)->renderer)
+        {
+            SDL_DestroyRenderer((*ctx)->renderer);
+            (*ctx)->renderer = NULL;
+        }
+
+        if ((*ctx)->window)
+        {
+            SDL_DestroyWindow((*ctx)->window);
+            (*ctx)->window = NULL;
+        }
+
+        SDL_free(*ctx);
+        *ctx = NULL;
     }
 
-    AppContext *ctx = (AppContext *)appstate;
+    log_cleanup();
 
-    return sm_screen_event(&ctx->screen_manager, event);
+    SDL_Quit();
 }
 
 /**
- * SDL callback function which is call in the game-loop / rendering (once a frame)
+ * The method processes pending events. It stops at the first event that is not
+ * SDL_APP_CONTINUE.
  */
-SDL_AppResult SDL_AppIterate(void *appstate)
+static SDL_AppResult _app_event(AppContext *ctx)
 {
-    SDL_AppResult res;
-    AppContext *ctx = (AppContext *)appstate;
-    ScreenManager *sm = &ctx->screen_manager;
+    SDL_Event event;
 
-    //
-    // Process pending screen changes
-    //
-    sm_process_change(sm, ctx->renderer);
+    while (SDL_PollEvent(&event))
+    {
+        if (event.type == SDL_EVENT_QUIT)
+        {
+            return SDL_APP_SUCCESS;
+        }
+        else
+        {
+            SDL_AppResult res = sm_screen_event(&event);
+            if (res != SDL_APP_CONTINUE)
+            {
+                return res;
+            }
+        }
+    }
+    return SDL_APP_CONTINUE;
+}
 
-    //
-    // Compute delta time
-    //
+/**
+ * The function processes the update step.
+ */
+static SDL_AppResult _app_update(AppContext *ctx)
+{
     uint64_t now = SDL_GetTicks();
     double delta_time = (double)(now - ctx->last_time) / 1000.0;
     ctx->last_time = now;
 
-    //
-    // Update with delta time
-    //
-    res = sm_screen_update(sm, delta_time);
-    if (res != SDL_APP_CONTINUE)
-    {
-        return res;
-    }
+    return sm_screen_update(delta_time);
+}
 
-    //
-    // Rendering
-    //
+/**
+ * The function processes the rendering.
+ */
+static SDL_AppResult _app_render(AppContext *ctx)
+{
     SDL_SetRenderDrawColor(ctx->renderer, 0, 0, 0, 255);
     SDL_RenderClear(ctx->renderer);
 
-    res = sm_screen_render(sm, ctx->renderer);
-    if (res != SDL_APP_CONTINUE)
+    SDL_AppResult render_res = sm_screen_render(ctx->renderer);
+    if (render_res != SDL_APP_CONTINUE)
     {
-        return res;
+        return render_res;
     }
 
     SDL_RenderPresent(ctx->renderer);
+
     return SDL_APP_CONTINUE;
 }
 
 /**
- * SDL callback function to cleanup before quit.
+ * Game loop which is called from the emscripten by requestAnimationFrame
  */
-void SDL_AppQuit(void *appstate, SDL_AppResult result)
+static void _main_loop_step(void *arg)
 {
+    AppContext **ctx = (AppContext **)arg;
 
-    if (result == SDL_APP_FAILURE)
+    //
+    // Process the events
+    //
+    if (SDL_APP_CONTINUE != _app_event(*ctx))
     {
-        SDL_Log("App finished due to a failure!");
+        _app_cleanup(ctx);
+        emscripten_cancel_main_loop();
+        return;
     }
-    if (appstate)
+
+    //
+    // Change the sceen
+    //
+    sm_process_change((*ctx)->renderer);
+
+    //
+    // Update the app
+    //
+    if (SDL_APP_CONTINUE != _app_update(*ctx))
     {
-        AppContext *ctx = (AppContext *)appstate;
-
-        sm_cleanup(&ctx->screen_manager);
-
-        SDL_DestroyRenderer(ctx->renderer);
-        SDL_DestroyWindow(ctx->window);
-        SDL_free(ctx);
+        _app_cleanup(ctx);
+        emscripten_cancel_main_loop();
+        return;
     }
-    SDL_Quit();
+
+    //
+    // Render the app
+    //
+    if (SDL_APP_CONTINUE != _app_render(*ctx))
+    {
+        _app_cleanup(ctx);
+        emscripten_cancel_main_loop();
+        return;
+    }
+}
+
+/**
+ * Main function for WebAssembly
+ */
+int main(int argc, char *argv[])
+{
+    (void)argc;
+    (void)argv;
+
+    AppContext *ctx = NULL;
+
+    if (SDL_APP_CONTINUE != _app_init(&ctx))
+    {
+        _app_cleanup(&ctx);
+        return 1;
+    }
+
+    //
+    // Hands over the control to the browser.
+    //
+    emscripten_set_main_loop_arg(_main_loop_step, &ctx, 0, 1);
+
+    //
+    // This area is not reachable.
+    //
+    return 0;
 }
