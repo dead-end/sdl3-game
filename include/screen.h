50d23af18a7372f897f862c0d736ca46da47a998
Dead End <dead.end@inbox.ru>
2026-09-22 11:51:29 +0200

    removed state

--- include/screen.h
+++ include/screen.h
@@ -6,20 +6,15 @@
 typedef struct Screen
 {
 
-    bool (*init)(void *state, SDL_Renderer *renderer);
+    SDL_AppResult (*init)(SDL_Renderer *renderer);
 
-    SDL_AppResult (*event)(void *state, SDL_Event *event);
+    SDL_AppResult (*event)(SDL_Event *event);
 
-    SDL_AppResult (*update)(void *state, double delta_time);
+    SDL_AppResult (*update)(double delta_time);
 
-    SDL_AppResult (*render)(void *state, SDL_Renderer *renderer);
+    SDL_AppResult (*render)(SDL_Renderer *renderer);
 
-    void (*cleanup)(void *state);
-
-    //
-    // Screen specific data
-    //
-    void *state;
+    void (*cleanup)();
 } Screen;
 
 #endif
